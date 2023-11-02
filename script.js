b2480e57c5f048456b6a2dc6518cec1ec7c96f6e
akashkrmishra <142215838+akashkrmishra@users.noreply.github.com>
2023-11-02 09:16:01 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,9 +1,10 @@
 function decimalToBinary(num) {
   //Write you code here
   return(num>>>0).toString(2);
-let num = 10;
+// let num = 10;
 let binary = decimalToBinary(num);
 return binary;
 }
 
-window.decimalToBinary = decimalToBinary;
+// window.decimalToBinary = decimalToBinary;
+console.log(decimalToBinary(10));
