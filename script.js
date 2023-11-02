1669bbd20f274523c2a2754cba50b55d194d789f
akashkrmishra <142215838+akashkrmishra@users.noreply.github.com>
2023-11-02 08:56:54 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,9 +1,9 @@
 function decimalToBinary(num) {
   //Write you code here
-	const number = parseInt(prompt('Enter a decimal number: '));
-const result = number.toString(2);
-console.log(`Binary:`+` `+ result);
-  return result;
+  return(num>>>0).toString(2);
+let num = 10;
+let binary = decimalToBinary(num);
+return binary;
 }
 
 window.decimalToBinary = decimalToBinary;
